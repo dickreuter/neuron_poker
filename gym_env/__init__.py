3a71a1a9648ea4a5a7b190755edc2e23e3ffb1aa
st-tse <st624@cornell.edu>
2021-04-01 20:56:53 -0400

    adding compatibility with alternate rewards

--- gym_env/__init__.py
+++ gym_env/__init__.py
@@ -3,3 +3,9 @@ from gym.envs.registration import register
 
 register(id='neuron_poker-v0',
          entry_point='gym_env.env:HoldemTable')
+
+register(id='neuron_poker-v1',
+         entry_point='gym_env.env_2:HoldemTable')
+
+register(id='neuron_poker-v2',
+         entry_point='gym_env.env_3:HoldemTable')
