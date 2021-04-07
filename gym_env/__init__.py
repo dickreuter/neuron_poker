8b2693f13dce5cddef953b753d0237e21ae31ce0
st-tse <st624@cornell.edu>
2021-04-06 21:23:03 -0400

    found bug reverting this

--- gym_env/__init__.py
+++ gym_env/__init__.py
@@ -3,3 +3,6 @@ from gym.envs.registration import register
 
 register(id='neuron_poker-v0',
          entry_point='gym_env.env:HoldemTable')
+
+register(id='neuron_poker-v1',
+         entry_point='gym_env.env_v1:HoldemTable')
--- gym_env/env-v1.py
+++ gym_env/env-v1.py
@@ -156,7 +156,7 @@ class HoldemTable(Env):
         self.funds_history = None
         self.array_everything = None
         self.legal_moves = None
-        self.illegal_move_reward = -1
+        self.illegal_move_reward = -1_000_000
         self.action_space = Discrete(len(Action) - 2)
         self.first_action_for_hand = None
 
