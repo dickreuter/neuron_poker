d8aafb14674c0e6d7d893c75053dacad1293ea9c
st-tse <st624@cornell.edu>
2021-04-06 20:59:18 -0400

    fixed renamed env might change naming later

--- gym_env/__init__.py
+++ gym_env/__init__.py
@@ -5,7 +5,7 @@ register(id='neuron_poker-v0',
          entry_point='gym_env.env:HoldemTable')
 
 register(id='neuron_poker-v1',
-         entry_point='gym_env.env_2:HoldemTable')
+         entry_point='gym_env.env_1:HoldemTable')
 
 register(id='neuron_poker-v2',
-         entry_point='gym_env.env_3:HoldemTable')
+         entry_point='gym_env.env_2:HoldemTable')
