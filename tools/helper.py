7d375d822c461bafa790a5f1e23fed73f564a912
Shmulik Cohen <anuk909@gmail.com>
2025-08-04 01:51:16 +0300

    fix pylint and CI

--- tools/helper.py
+++ tools/helper.py
@@ -181,8 +181,7 @@ def flatten(items):
     """Yield items from any nested iterable; see Reference."""
     for x in items:
         if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
-            for sub_x in flatten(x):
-                yield sub_x
+            yield from flatten(x)
         else:
             yield x
 
